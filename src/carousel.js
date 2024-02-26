aff8fbb9b8d08f06e42784fc2417bd030a206306
Neo319 <alexmnevins@gmail.com>
2024-02-26 10:23:48 -0500

    create base for image slider

---
+++ src/carousel.js
@@ -0,0 +1,18 @@
+// module for creating a sliding image carousel given a div of id 'carouselContainer'
+// div must contain another div of id 'slider', with images of class 'carouselImg'
+
+const carousel = function () {
+    console.log('carousel ready!')
+
+    const container = document.getElementById('carouselContainer')
+    const slider = document.getElementById('slider')
+    const allImages = document.getElementsByClassName('carouselImg')
+
+    function right () {
+        
+    }
+
+
+}
+
+export default carousel
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import dropdown from "./dropdown"
+import carousel from "./carousel"
 
 import "./style.css"
 
 console.log("index.js ready!")
 dropdown()
+
+carousel()

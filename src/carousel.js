778aaf48c40bf64c82103665beffbda270397d38
Neo319 <alexmnevins@gmail.com>
2024-02-26 15:07:04 -0500

    navigation circles implemented

--- src/carousel.js
+++ src/carousel.js
@@ -18,28 +18,41 @@ const carousel = function () {
 
     //create a menu of divs that appear as circles to cycle through images 
     for (let i = 0; i <= imageCount; i++) {
+        const newDivContainer = document.createElement('div')
         const newDiv = document.createElement('div')
+        newDivContainer.appendChild(newDiv)
         newDiv.classList.add('tabCircle')
 
-        newDiv.addEventListener('click', () => {
-            console.log(`selecting image ${i}`)
+        newDivContainer.addEventListener('click', () => {
             currentImage = i // the displayed image index is stored
             changeImage(currentImage) // the image is changed
         })
-        tabs.appendChild(newDiv)
+
+        tabs.appendChild(newDivContainer)
     }
+
+    container.after(tabs) //tabs are appended after the slider container
 
     // a dot that indicates which image is currently displayed
     const dot = document.createElement('div')
-    dot.id = dot
+    dot.id = 'dot'
+
+    const firstCircle = document.querySelector('.tabCircle')
+    console.log(firstCircle)
+    firstCircle.appendChild(dot) // default positioning is inside the first circle
     
-    container.after(tabs)
+    
 
     
     function changeImage (index) {
+        console.log(`selecting ${index}`)
         //changing image logic
 
         //moving dot logic
+        const circleGap = 24 //the distance to move by one circle, in px
+        dot.style.transform = `translateX(${circleGap * currentImage}px)` 
+        
+        dot.classList.add('transition')
     }
     
 

41dae756a9afa04cc6c685cdec1937b268ac5432
Neo319 <alexmnevins@gmail.com>
2024-02-26 11:36:55 -0500

    sliding right base level functions implemented

--- src/carousel.js
+++ src/carousel.js
@@ -8,9 +8,33 @@ const carousel = function () {
     const slider = document.getElementById('slider')
     const allImages = document.getElementsByClassName('carouselImg')
 
+    let currentImage = 0 // the index of the image currently displayed
+    const imageCount = allImages.length - 1 // the number of images that can be scrolled through
+
+
+
     function right () {
-        
+        // set a class on div #slider that activates a CSS animation, then remove it with a timeout 
+        console.log('sliding right')
+        if (currentImage !== imageCount) { // if not scrolled to the far right
+            currentImage++
+            console.log(currentImage)
+            
+            const imageWidth = allImages[currentImage].offsetWidth 
+            slider.classList.add('sliding-right') //used to trigger an animation
+            slider.style.transform = `TranslateX(${-imageWidth * currentImage}px)`
+
+        } else {
+            console.log('too far right') // will be animated
+        }
     }
+    
+    //button to move right
+    const rightBtn = document.createElement('button')
+    rightBtn.textContent = '>' // temp styling
+    rightBtn.addEventListener('click', right)
+
+    container.appendChild(rightBtn) 
 
 
 }
